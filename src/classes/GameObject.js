9ef513292fa094b4a73a2eb6a76726b2554f9c2b
AndyBoltonMV <andrew.bolton@multiverse.io>
2022-10-20 09:51:02 +0100

    created file structure, populated basic types and class definition

---
+++ src/classes/GameObject.js
@@ -0,0 +1,29 @@
+/** @class - Represents a Game Object. */
+class GameObject {
+  /**
+   * Create a Game Object.
+   * @param {Object} config - Configuration values for class
+   * @param {string} config.src - Image source
+   * @param {Array.<number,number>} config.location - Array containing x & y coordinates
+   */
+  constructor(config) {
+    this.src = config.src;
+    this.location = config.location;
+  }
+  /**
+   * Get the location coordinates
+   * @return {number[]} The location coordinates
+   */
+  get location() {
+    return this.location;
+  }
+  /**
+   * Update the location coordinates
+   * @param {Array.<number, number>} newLocation - Updated coordinates
+   */
+  set location(newLocation) {
+    this.location = newLocation;
+  }
+}
+
+module.exports = GameObject;
